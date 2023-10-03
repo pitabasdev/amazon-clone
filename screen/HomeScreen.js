028afb53822ecd134c843e8a69a932e1c55eb045
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2023-10-03 20:13:57 +0530

    Import FakestoreApi

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,8 +1,9 @@
 import { StyleSheet, Text, View, SafeAreaView, Platform, ScrollView, Image, Pressable, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { SliderBox } from "react-native-image-slider-box";
 import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
+import DropDownPicker from "react-native-dropdown-picker";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 const HomeScreen = () => {
@@ -112,6 +113,87 @@ const HomeScreen = () => {
             ],
         },
     ];
+    const offers = [
+        {
+            id: "0",
+            title:
+                "Oppo Enco Air3 Pro True Wireless in Ear Earbuds with Industry First Composite Bamboo Fiber, 49dB ANC, 30H Playtime, 47ms Ultra Low Latency,Fast Charge,BT 5.3 (Green)",
+            offer: "72% off",
+            oldPrice: 7500,
+            price: 4500,
+            image:
+                "https://m.media-amazon.com/images/I/61a2y1FCAJL._AC_UL640_FMwebp_QL65_.jpg",
+            carouselImages: [
+                "https://m.media-amazon.com/images/I/61a2y1FCAJL._SX679_.jpg",
+                "https://m.media-amazon.com/images/I/71DOcYgHWFL._SX679_.jpg",
+                "https://m.media-amazon.com/images/I/71LhLZGHrlL._SX679_.jpg",
+                "https://m.media-amazon.com/images/I/61Rgefy4ndL._SX679_.jpg",
+            ],
+            color: "Green",
+            size: "Normal",
+        },
+        {
+            id: "1",
+            title:
+                "Fastrack Limitless FS1 Pro Smart Watch|1.96 Super AMOLED Arched Display with 410x502 Pixel Resolution|SingleSync BT Calling|NitroFast Charging|110+ Sports Modes|200+ Watchfaces|Upto 7 Days Battery",
+            offer: "40%",
+            oldPrice: 7955,
+            price: 3495,
+            image: "https://m.media-amazon.com/images/I/41mQKmbkVWL._AC_SY400_.jpg",
+            carouselImages: [
+                "https://m.media-amazon.com/images/I/71h2K2OQSIL._SX679_.jpg",
+                "https://m.media-amazon.com/images/I/71BlkyWYupL._SX679_.jpg",
+                "https://m.media-amazon.com/images/I/71c1tSIZxhL._SX679_.jpg",
+            ],
+            color: "black",
+            size: "Normal",
+        },
+        {
+            id: "2",
+            title: "Aishwariya System On Ear Wireless On Ear Bluetooth Headphones",
+            offer: "40%",
+            oldPrice: 7955,
+            price: 3495,
+            image: "https://m.media-amazon.com/images/I/41t7Wa+kxPL._AC_SY400_.jpg",
+            carouselImages: ["https://m.media-amazon.com/images/I/41t7Wa+kxPL.jpg"],
+            color: "black",
+            size: "Normal",
+        },
+        {
+            id: "3",
+            title:
+                "Fastrack Limitless FS1 Pro Smart Watch|1.96 Super AMOLED Arched Display with 410x502 Pixel Resolution|SingleSync BT Calling|NitroFast Charging|110+ Sports Modes|200+ Watchfaces|Upto 7 Days Battery",
+            offer: "40%",
+            oldPrice: 24999,
+            price: 19999,
+            image: "https://m.media-amazon.com/images/I/71k3gOik46L._AC_SY400_.jpg",
+            carouselImages: [
+                "https://m.media-amazon.com/images/I/41bLD50sZSL._SX300_SY300_QL70_FMwebp_.jpg",
+                "https://m.media-amazon.com/images/I/616pTr2KJEL._SX679_.jpg",
+                "https://m.media-amazon.com/images/I/71wSGO0CwQL._SX679_.jpg",
+            ],
+            color: "Norway Blue",
+            size: "8GB RAM, 128GB Storage",
+        },
+    ];
+    const [products, setProducts] = useState([]);
+
+
+
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("https://fakestoreapi.com/products");
+                setProducts(response.data);
+            } catch (error) {
+                console.log("error message", error);
+            }
+        };
+
+        fetchData();
+    }, []);
     return (
         <SafeAreaView
             style={{
@@ -256,7 +338,100 @@ const HomeScreen = () => {
                 <Text style={{ padding: 10, fontSize: 18, fontWeight: "bold" }}>
                     Today's Deals
                 </Text>
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                    {offers.map((item, index) => (
+                        <Pressable
+                            onPress={() =>
+                                navigation.navigate("Info", {
+                                    id: item.id,
+                                    title: item.title,
+                                    price: item?.price,
+                                    carouselImages: item.carouselImages,
+                                    color: item?.color,
+                                    size: item?.size,
+                                    oldPrice: item?.oldPrice,
+                                    item: item,
+                                })
+                            }
+                            style={{
+                                marginVertical: 10,
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <Image
+                                style={{ width: 150, height: 150, resizeMode: "contain" }}
+                                source={{ uri: item?.image }}
+                            />
 
+                            <View
+                                style={{
+                                    backgroundColor: "#E31837",
+                                    paddingVertical: 5,
+                                    width: 130,
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    marginTop: 10,
+                                    borderRadius: 4,
+                                }}
+                            >
+                                <Text
+                                    style={{
+                                        textAlign: "center",
+                                        color: "white",
+                                        fontSize: 13,
+                                        fontWeight: "bold",
+                                    }}
+                                >
+                                    Upto {item?.offer}
+                                </Text>
+                            </View>
+                        </Pressable>
+                    ))}
+                </ScrollView>
+                <Text
+                    style={{
+                        height: 1,
+                        borderColor: "#D0D0D0",
+                        borderWidth: 2,
+                        marginTop: 15,
+                    }}
+                />
+                <View
+                    style={{
+                        marginHorizontal: 10,
+                        marginTop: 20,
+                        width: "45%",
+                        marginBottom: 50,
+                    }}
+                >
+                    {/* <DropDownPicker
+                        style={{
+                            borderColor: "#B7B7B7",
+                            height: 30,
+                            marginBottom: open ? 120 : 15,
+                        }}
+                        open={open}
+                        value={category} //genderValue
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setCategory}
+                        setItems={setItems}
+                        placeholder="choose category"
+                        placeholderStyle={styles.placeholderStyles}
+                        onOpen={onGenderOpen}
+                        // onChangeValue={onChange}
+                        zIndex={3000}
+                        zIndexInverse={1000}
+                    /> */}
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                            flexWrap: "wrap",
+                        }}
+                    ></View>
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
